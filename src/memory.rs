6ac78e1bd93e2729e4bd9d55d9c3f5e2c6f0704f
leopnt <leopnt.dev@gmail.com>
2022-06-07 17:26:51 +0200

    add font loading

--- src/memory.rs
+++ src/memory.rs
@@ -2,6 +2,7 @@ const MAX_SIZE: u16 = 0x1000;
 
 const PROG_LOC: u16 = 0x0200;
 pub const DISPLAY_LOC: u16 = 0x0F00;
+pub const FONT_LOC: u16 = 0x0050;
 
 pub struct Memory {
     data: [u8; MAX_SIZE as usize],
@@ -29,6 +30,12 @@ impl Memory {
     pub fn load_prog(&mut self, prgm: &[u8]) {
         for (i, byte) in prgm.iter().enumerate() {
             self.data[(PROG_LOC as usize + i)] = *byte;
+        }
+    }
+
+    pub fn load_font(&mut self, font: &[u8]) {
+        for (i, byte) in font.iter().enumerate() {
+            self.data[(FONT_LOC as usize + i)] = *byte;
         }
     }
 
@@ -82,6 +89,35 @@ mod tests {
 
         for i in 0..data.len() {
             assert_eq!(data[i], mem.read(PROG_LOC + i as u16));
+        }
+    }
+
+    #[test]
+    fn test_load_font() {
+        let font = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+        ];
+
+        let mut mem = Memory::new();
+        mem.load_font(&font);
+
+        for i in 0..font.len() {
+            assert_eq!(font[i], mem.read(FONT_LOC + i as u16));
         }
     }
 }
